93719eb73cd170637d659bdaf3d6fa4767bac6b3
Andrew <andretis990@gmail.com>
2018-04-27 23:20:50 +0300

    Встроил класс сообщений и список сообщений.

--- lib/ChatClass.dart
+++ lib/ChatClass.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'ChatMessage.dart';
 
 class FriendlychatApp extends StatelessWidget {
   @override
@@ -18,8 +19,15 @@ class ChatScreen extends StatefulWidget {                     //modified
 // Add the ChatScreenState class definition in main.dart.
 
 class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];             // new
   void _handleSubmitted(String text) {
     _textController.clear();
+    ChatMessage message = new ChatMessage(                         //new
+      text: text,                                                  //new
+    );                                                             //new
+    setState(() {                                                  //new
+      _messages.insert(0, message);                                //new
+    });                                                            //new
   }
 
   final TextEditingController _textController = new TextEditingController(); //new
@@ -30,7 +38,25 @@ class ChatScreenState extends State<ChatScreen> {
       appBar: new AppBar(
           title: new Text("Friendlychat")
       ),
-      body: _buildTextComposer(), //new
+      body: new Column(                                        //modified
+        children: <Widget>[                                         //new
+          new Flexible(                                             //new
+            child: new ListView.builder(                            //new
+              padding: new EdgeInsets.all(8.0),                     //new
+              reverse: true,                                        //new
+              itemBuilder: (_, int index) => _messages[index],      //new
+              itemCount: _messages.length,                          //new
+            ),                                                      //new
+          ),                                                        //new
+          new Divider(height: 1.0),                                 //new
+          new Container(                                            //new
+            decoration: new BoxDecoration(
+                color: Theme.of(context).cardColor),                  //new
+            child: _buildTextComposer(),                       //modified
+          ),                                                        //new
+        ],                                                          //new
+      ),                                                            //new
+
     );
   }
 
